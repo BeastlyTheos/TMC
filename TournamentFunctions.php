03d536120097a632d23fae36a567337f724cdffe
Ted <TheodoreCooke@trentu.ca>
2017-10-17 10:21:09 -0400

    update start_of_season constant

--- TournamentFunctions.php
+++ TournamentFunctions.php
@@ -1,5 +1,5 @@
 <?$today = new DateTimeImmutable();
-$startOfNationalCup = new DateTimeImmutable("2016-02-11"); //thursday  before first national cup match
+$startOfNationalCup = new DateTimeImmutable("2017-08-24"); //thursday  before first national cup match
 define("CurrentRound", CupRoundOf($today));
 
 function CupRoundOf($date)
