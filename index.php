5ea408e94d8d8267a3c49d7b8b8bd9338609d592
Ted <TheodoreCooke@trentu.ca>
2017-10-18 05:05:52 -0400

    add admin panel

---
+++ index.php
@@ -0,0 +1,16 @@
+<title>admin panel</title>
+<h1>Admin Panel</h1>
+<h2>registration</h2>
+<a href="register.php?edition=5&context=11">register</a>
+
+<h2>before matches</h2>
+<a href="cup.php">check for cup leavers</a>
+<a href="CreateFixtures.php?c=11">Create Fixtures</a>
+<a href="fixtures.php?e=5">view fixtures</a>
+<a href="matchIDs.php">get match IDs</a>
+a href="htlive.php?e=5">HT-Live</a>
+
+<h2>after matches</h2>
+<a href="PlayMatches.php">get match results</a>
+<a href="fixtures.php?e=5">view fixtures</a>
+a href="standings.php?c=11">standings</a>
