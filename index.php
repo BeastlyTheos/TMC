e3f4d543b42dac56b67bbb43ba1b348f8fe9f520
Ted <TheodoreCooke@trentu.ca>
2017-10-25 04:20:44 -0400

    templatise index.php

--- index.php
+++ index.php
@@ -1,16 +1,7 @@
-<title>admin panel</title>
-<h1>Admin Panel</h1>
-<h2>registration</h2>
-<a href="register.php?edition=5&context=11">register</a>
+<?php $debug = true;
+require_once 'C:/xampp/vendor/autoload.php';
+  $loader = new Twig_Loader_Filesystem('templates');
+ $twig = new Twig_Environment($loader, array("debug"=>$debug));
 
-<h2>before matches</h2>
-<a href="check_cup_eliminations.php">check for cup leavers</a>
-<a href="create_fixtures.php?c=11">Create Fixtures</a>
-<a href="view_fixtures.php?e=5">view fixtures</a>
-<a href="get_match_IDs.php">get match IDs</a>
-a href="get_HT-Live_URL.php?e=5">HT-Live</a>
-
-<h2>after matches</h2>
-<a href="get_match_results.php">get match results</a>
-<a href="view_fixtures.php?e=5">view fixtures</a>
-<a href="view_standings.php?c=11">standings</a>
+$twig->display("index.html");
+?>
