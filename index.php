edfd607236f29deac6b4151252b4e1c50dc7fbeb
Ted <TheodoreCooke@trentu.ca>
2017-10-27 12:10:07 -0400

    standardise most filenames

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
 <a href="register.php?edition=5&context=11">register</a>
 
 <h2>before matches</h2>
-<a href="cup.php">check for cup leavers</a>
-<a href="CreateFixtures.php?c=11">Create Fixtures</a>
-<a href="fixtures.php?e=5">view fixtures</a>
-<a href="matchIDs.php">get match IDs</a>
-a href="htlive.php?e=5">HT-Live</a>
+<a href="check_cup_eliminations.php">check for cup leavers</a>
+<a href="create_fixtures.php?c=11">Create Fixtures</a>
+<a href="view_fixtures.php?e=5">view fixtures</a>
+<a href="get_match_IDs.php">get match IDs</a>
+a href="get_HT-Live_URL.php?e=5">HT-Live</a>
 
 <h2>after matches</h2>
-<a href="PlayMatches.php">get match results</a>
-<a href="fixtures.php?e=5">view fixtures</a>
-a href="standings.php?c=11">standings</a>
+<a href="get_match_results.php">get match results</a>
+<a href="view_fixtures.php?e=5">view fixtures</a>
+<a href="view_standings.php?c=11">standings</a>
