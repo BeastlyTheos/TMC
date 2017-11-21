6b5ae2c25d35612ca76dbfa9e63de0624bd5cf55
Ted <TheodoreCooke@trentu.ca>
2017-11-21 01:14:20 -0500

    add date format  to the block for iterating through away's friendlies

--- get_match_IDs.php
+++ get_match_IDs.php
@@ -59,7 +59,7 @@ $res = yoursql_query("select home, away, h.name as homeName, a.name as awayName,
 			{if($matches = getTeamMatchesByRound($r["away"], $r["round"]))
 				{
 				/* find away's HT matches, and cycle through them */
-				for ($i = $matches->getNumberMatches() ; ! $awayTeamHasFriendly && null != ($m = $matches->getMatch($i)) && $m->getDate() >= startOfNationalCupWeek($r["round"]) ; $i--) //for each match
+				for ($i = $matches->getNumberMatches() ; !$awayTeamHasFriendly &&   null != ($m = $matches->getMatch($i)) && $m->getDate() >= startOfNationalCupWeek($r["round"])->format(dateFormat) ; $i--) //for each match
 					{$m = $matches->getMatch($i);
 					//printf("considering the match between %s and %s<br/>\n", $m->getHomeTeamName(), $m->getAwayTeamName());
 					if(4 <= $m->getType() && $m->getType() <= 9 && $m->getType() != 8) //if is a friendly
