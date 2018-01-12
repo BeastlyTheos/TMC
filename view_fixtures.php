a1eef9c74105f4e7c84cb224471946c36cd253fe
Ted <TheodoreCooke@trentu.ca>
2018-01-12 05:44:35 -0500

    refactor View_Fixtures.php to create table data to pass into a template
    Template not yet created

--- view_fixtures.php
+++ view_fixtures.php
@@ -1,55 +1,72 @@
-﻿<?php
-$db = 'tournament';
+﻿<?php $debug = true;
+require_once 'C:/xampp/vendor/autoload.php';
+  $loader = new Twig_Loader_Filesystem('templates');
+ $twig = new Twig_Environment($loader, array("debug"=>$debug));
+
 include 'yoursql.php';
 include 'TournamentFunctions.php';
-$edition = (int) $_GET['e'];
+
+$editionID = (int) $_GET['e'];
 if(isset($_GET['r']))
 	$round = (int) $_GET['r'];
 else $round = CupRoundOf($today);
 if ( CurrentRound > $round)
 	$hasCompleted = true;
-else $hasCompleted = false;
-$res = yoursql_query("select id from contexts where edition = $edition and id in (select distinct(context) from matches where round = $round)");
-
-echo "<title>edition $edition, round $round, </title>";
-
-//echo "[u] Round ".$round-$context['startingOffset']."[/u]<br/>";
-
-if($res->num_rows)
-	while($r = $res->fetch_assoc())//if there is a result
-		PrintMatchesByContext( $r['id'], $round);
 else
-	printf("No matches in round $round");
+	$hasCompleted = false;
+$contexts = yoursql_query("select * from contexts where edition = $editionID and id in (select distinct(context) from matches where round = $round)");
+$tables = array();
 
-function HTClub($name, $id)
-{return "$name [teamid=$id]";}
 
-function PrintMatchesByContext( $contextId, $round)
-{global $hasCompleted;
-$context = yoursql_query("select * from contexts where id = $contextId")->fetch_assoc();
-$matches = yoursql_query("call getMatchesByContext(${context['id']},$round)");
+while ( $context = $contexts->fetch_assoc() )
+	{
+		$matches = yoursql_query("call getMatchesByContext( {$context['id']}, $round)");
+	$usesNeutralVenues = $context['usesNeutralVenues'];
+	$rows = array();
 
-$neutral = $context['usesNeutralVenues'];
-$columnHeader1 = $neutral? "Team": "Home";
-$columnHeader2 = "Score";
-$columnHeader3 = $neutral? "Team": "Away";
-$columnHeader4 = $hasCompleted? "matchid": "suggested arena id";
+	$headers = array();
+	if ( $usesNeutralVenues )
+		$headers[] = "Team1";
+	else
+		$headers[] = "Home";
+	if ( $hasCompleted )
+		$headers[] = "Score";
+	if ( $usesNeutralVenues )
+		$headers[] = "Team2";
+	else
+		$headers[] = "away";
+	if ( $hasCompleted )
+		$headers[] = "Match IDs";
+	else if ( $usesNeutralVenues )
+		$headers[] = "Suggested Arena ID";
 
-echo '[table]<br/>';
-printf("[tr][td colspan=%d]${context['name']}[/td][/tr]<br/>", $hasCompleted?4:3);
-printf("[tr][th]%s[/th][th]%s[/th][th]%s[/th][th]%s[/th][/tr]<br/>",
-	$columnHeader1, $columnHeader2, $columnHeader3, $columnHeader4);
+	//load table data
+	foreach ( $matches as $m )
+		{
+		$row = array();
 
-while ($m = $matches->fetch_assoc()) //print all matches while in this context
-	{$cell1 = HTClub($m['homeName'], $m['homeId']);
-$cell2 = fnmatch('*forfit', $m['type'])? $m['type']: null === $m['homegoals']? ' __ - __ ': sprintf(' %.2d - %.2d ', $m['homegoals'], $m['awaygoals']);
-$cell3 = 		'bye'==$m['type']? 'bye        ': HTClub($m['awayName'], $m['awayId']);
-$cell4 = $m['matchId']? "[matchid=${m['matchId']}]": "${m['arena']}";
+		//load the name of the home team
+		$row [] = $m["homeName"];
 
-printf("[tr][td]%s[/td][td]%s[/td][td]%s[/td][td]%s[/td][/tr]<br>\n",
-$cell1, $cell2, $cell3, $cell4);
-}//end while there is a match to print
+		//load score if the match has happened
+		if ( $hasCompleted )
+			$row[] = sprintf("%d %d", $m["homegoals"], $m["awaygoals"]);
 
-echo '[/table]<br/>';
-}
+		//load the names of the away teams
+		$row[] = $m["awayName"];
+
+		//load either the match id or the areana id
+		if ( $hasCompleted )
+			$row[] = $m["matchId"];
+		else if ( $usesNeutralVenues )
+			$row[] = $m["arena"];
+
+		$rows[] = $row;
+		}//end for each match
+
+	//add row to table
+	$tables[$context["name"]] = $rows;
+	}//end iterating through contexts
+
+var_dump($tables);
 ?>
