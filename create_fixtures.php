f589eb069f4daa62545f4e584d20d2905d5de1e7
Ted <TheodoreCooke@trentu.ca>
2018-05-21 12:45:15 -0400

    create fixtures gracefully handles no available teams

--- create_fixtures.php
+++ create_fixtures.php
@@ -16,33 +16,37 @@ $context = $_GET['c'];
 $res = yoursql_query("call getStandingsWithoutForfits($context)");
 $numTeams = $res->num_rows;
 $matches = new SplStack();
+$teams = Array();
 
-//load the teams from the sql result into the teams array
-for ($i = 0; $i < $numTeams; $i++)
-	{$r = $res->fetch_assoc();
+if ( 0 != $numTeams )
+	{
+	//load the teams from the sql result into the teams array
+	for ($i = 0; $i < $numTeams; $i++)
+		{$r = $res->fetch_assoc();
 	$teams[$i] = new Team($r['id'], $i, $r['name'], $r['gp'], $r['byes']);
-	}
-
-$matches = pairing_state_machine::create($teams, $context);
-usort($matches, 'teamcmp');
-
-            //load $matches into sql
-$sql->begin_transaction();
-foreach ( $matches as $m )
-	{//as for convention, insure that the home team is not a bye
-	if (0 == $m->home->id)
-		{
-		$temp = $m->home;
-		$m->home = $m->away;
-		$m->away = $temp;
 		}
 
-	if (0 == $m->away->id)
-		yoursql_query("call scheduleBye( " . $m->home->id . ", $context, $upcomingRound)");
-	else
-		yoursql_query('call ScheduleMatch('.$m->home->id.', '.$m->away->id.", $context, $upcomingRound)");
-	}//END EACH MATCH
-$sql->commit();
+	$matches = pairing_state_machine::create($teams, $context);
+	usort($matches, 'teamcmp');
+
+	            //load $matches into sql
+	$sql->begin_transaction();
+	foreach ( $matches as $m )
+		{//as for convention, insure that the home team is not a bye
+		if (0 == $m->home->id)
+			{
+			$temp = $m->home;
+			$m->home = $m->away;
+			$m->away = $temp;
+			}
+
+		if (0 == $m->away->id)
+			yoursql_query("call scheduleBye( " . $m->home->id . ", $context, $upcomingRound)");
+		else
+			yoursql_query('call ScheduleMatch('.$m->home->id.', '.$m->away->id.", $context, $upcomingRound)");
+		}//END EACH MATCH
+	$sql->commit();
+	}
 
 $twig->display( "create_fixtures.html", array(
 	"teams"=>$teams,
