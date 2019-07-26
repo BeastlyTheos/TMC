d3f7bc883ef6c27aea8b0545c065621bc04e4f10
Ted <TheodoreCooke@trentu.ca>
2019-07-25 21:15:23 -0400

    add entrants.php

---
+++ entrants.php
@@ -0,0 +1,17 @@
+<?php $debug = true;
+require_once 'C:/xampp/vendor/autoload.php';
+  $loader = new Twig_Loader_Filesystem('templates');
+ $twig = new Twig_Environment($loader, array("debug"=>$debug));
+
+include "yoursql.php";
+include "TournamentFunctions.php";
+
+$res = yoursql_query("select teams.id as id, teams.name as name, seed from teams join entrants on teams.id = entrants.id where edition = $edition");
+$teams = $res->fetch_all(1);
+
+$twig->display("entrants.html", array(
+	"edition"=>$edition,
+	"teams"=>$teams,
+	)
+);
+?>
