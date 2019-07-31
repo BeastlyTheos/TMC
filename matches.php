c74bdffcb348cb4cf746732963178f47b3ad4631
Ted <TheodoreCooke@trentu.ca>
2019-07-31 10:37:09 -0400

    add matches.php

---
+++ matches.php
@@ -0,0 +1,19 @@
+<?php $debug = true;
+require_once 'C:/xampp/vendor/autoload.php';
+  $loader = new Twig_Loader_Filesystem('templates');
+ $twig = new Twig_Environment($loader, array("debug"=>$debug));
+
+include "yoursql.php";
+include "TournamentFunctions.php";
+include "match.php";
+
+$res = yoursql_query("select * from matches where context = $context");
+$matches = [];
+while ($data=$res->fetch_assoc())
+	$matches[] = Match::createFromArray($data);
+
+$twig->display("matches.html", array(
+	"matches"=>$matches,
+	)
+);
+?>
