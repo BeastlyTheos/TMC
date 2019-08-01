69fcb012d865ad06cf4bf9691c6dee673cf540f4
Ted <TheodoreCooke@trentu.ca>
2019-08-01 01:31:58 -0400

    add edit match

---
+++ edit_match.php
@@ -0,0 +1,39 @@
+<?php $debug = true;
+require_once 'C:/xampp/vendor/autoload.php';
+  $loader = new Twig_Loader_Filesystem('templates');
+ $twig = new Twig_Environment($loader, array("debug"=>$debug));
+
+include "yoursql.php";
+include "TournamentFunctions.php";
+include "match.php";
+
+//initialise the match object
+if ($_POST)
+	{//try to save the post data as a match
+$match = Match::createFromArray($_POST);
+	try {
+		$match->save();
+		header("Location: view_matches.php");
+		}
+	catch (Exception $e) 
+		{var_dump($e);}
+	}
+else 
+	{//get value of o
+	if (isset($_GET["o"]))
+		$o = strval($_GET["o"]);
+	else
+		$o = "0";
+	$match = Match::getByO($o);
+	}
+
+$res = yoursql_query("select teams.id, teams.name from teams join standings on teams.id = standings.id where context = $context");
+$teams = $res->fetch_all(1);
+$teams[] = Team::getByID(0);
+
+$twig->display("edit_match.html", array(
+	"match"=>$match,
+	"teams"=>$teams,
+	)
+);
+?>
