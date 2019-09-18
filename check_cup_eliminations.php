e3107634caca4d9f15e9921d6b53c85915e21554
Ted <TheodoreCooke@trentu.ca>
2019-09-18 18:56:52 -0400

    edit the path of autoload.php to be a relative dir instead of absolute path

--- check_cup_eliminations.php
+++ check_cup_eliminations.php
@@ -1,5 +1,5 @@
 <?php $debug = true;
-require_once 'C:/xampp/vendor/autoload.php';
+require_once 'vendor/autoload.php';
   $loader = new Twig_Loader_Filesystem('templates');
  $twig = new Twig_Environment($loader, array("debug"=>$debug));
 
